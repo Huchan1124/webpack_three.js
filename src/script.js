a94c0cf63657abc371d77a144077668a8176473d
alicia <garlic1124@gmail.com>
2022-04-12 14:30:16 +0800

    add tick animation

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
 import "./style.css"
 import * as THREE from "three"
 
+
 // DOM & sizes
 const canvas = document.querySelector('.webgl');
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 };
 
 
@@ -14,40 +15,21 @@ const scene = new THREE.Scene();
 
 
 // Create Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: '#8FE4F4' })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-const geometry = new THREE.CylinderGeometry(1, 1, 1, 32);
-const material = new THREE.MeshBasicMaterial({
-    color: '#8FE4F4'
-});
-
-
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-mesh.position.x = 1;
-mesh.position.y = 2;
-mesh.position.z = -2;
-
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
 
 
 //Create Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.set(0, 0, 5)
+
 
 scene.add(camera)
 
-console.log(mesh.position.length())
-console.log(mesh.position.distanceTo(camera.position))
-console.log(mesh.position.normalize())
 
-/**
- * Axes Helper
- */
-const axesHelper = new THREE.AxesHelper(3)
-scene.add(axesHelper)
 
 // Renderer
 
@@ -58,3 +40,32 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setClearColor("#006868", .8)
 renderer.render(scene, camera);
+
+
+/**
+ * Animate
+ */
+
+let time = Date.now();
+console.log(`time:${time}`);
+
+
+const tick = () => {
+
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+
+    time = currentTime;
+
+
+    // Update objects
+    mesh.rotation.y += 0.01 * deltaTime;
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
