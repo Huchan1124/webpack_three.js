30f699ec420e76edd451326c66de1438bb7143f3
alicia <garlic1124@gmail.com>
2022-04-13 15:41:59 +0800

    add resize

--- src/script.js
+++ src/script.js
@@ -83,3 +83,18 @@ const tick = () => {
 
 
 tick()
+
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+})
